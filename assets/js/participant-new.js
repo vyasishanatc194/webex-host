ea6fe39320baafba43d5a08a8e5c5a3620817fd2
Ishanvyas <vyasishanatc194@gmail.com>
2021-04-23 16:12:39 +0530

    Create participant design in inc all page

--- assets/js/participant-new.js
+++ assets/js/participant-new.js
@@ -3,20 +3,6 @@ $(document).ready(function () {
   function show_participants_btn() {
     $("#main-small-screen-view-area").addClass("show-area small-screen-area").removeClass("hidden-area");
     $("#show-participants-btn-area").addClass("hidden-area").removeClass("show-area");
-
-    // $("#main-small-screen-view-area").toggleClass("hidden-area");
-  
-    // $("#show-participants-btn-area").addClass("hidden-area");
-    // $("#main-small-screen-view-area").addClass("small-show-area");
-  
-    // $(".small-show-area #minimize-screen-btn").click(function(){
-    //   $("#main-small-screen-view-area.small-show-area").addClass("hidden-area").removeClass("show-area small-show-area");
-    //   $("#show-participants-btn-area").removeClass("hidden-area");
-    // });
-  
-    // $(".small-show-area #maximize-screen-btn").click(function(){
-    //   maximize_screen();
-    // });
   }
 
   function first_minimize_screen() {
@@ -25,14 +11,11 @@ $(document).ready(function () {
   }
 
   function maximize_screen() {
-
     $("#main-big-screen-view-area .part-list-root-div").addClass("show-area").removeClass("hidden-area");
     $("#goto-presentor-view-area").addClass("show-area").removeClass("hidden-area");
     $("#chat-group-participant-area").addClass("show-area").removeClass("hidden-area");
     $("#disabled-chat-btn").addClass("disabled-btn");
-  
-    // $("#main-small-screen-view-area").addClass("maximize-show-area").removeClass("small-show-area");
-    
+      
     $("#small-screen-image").addClass("hidden-area").removeClass("show-area");
     $("#small-screen-video-div").addClass("show-area").removeClass("hidden-area");
   
@@ -45,9 +28,7 @@ $(document).ready(function () {
     $("#goto-presentor-view-area").removeClass("show-area").addClass("hidden-area");
     $("#chat-group-participant-area").removeClass("show-area").addClass("hidden-area");
     $("#disabled-chat-btn").removeClass("disabled-btn");
-  
-    // $("#main-small-screen-view-area").addClass("maximize-show-area").removeClass("small-show-area");
-    
+      
     $("#small-screen-image").removeClass("hidden-area").addClass("show-area");
     $("#small-screen-video-div").removeClass("show-area").addClass("hidden-area");
   
@@ -61,8 +42,8 @@ $(document).ready(function () {
     $("#main-small-screen-view-area").addClass("maximize-show-area").removeClass("small-screen-area");
   }
 
-  function minimize_screen_add_class() {
-    $("#main-small-screen-view-area").addClass("small-screen-area").removeClass("maximize-show-area");
+  function show_participants_btn_not_show() {
+    $("#show-participants-btn-area").addClass("hidden-area").removeClass("show-area");
   }
 
   $("#show-participants-btn").click(function(){
@@ -78,10 +59,12 @@ $(document).ready(function () {
 
       $(".maximize-show-area #minimize-screen-btn").click(function(){
         minimize_screen();
+        show_participants_btn_not_show();
       });
 
       $(".maximize-show-area #goto-presentor-view-btn").click(function(){
         minimize_screen();
+        show_participants_btn_not_show();
       });
     });
   });
